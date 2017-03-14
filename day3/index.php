ff03fd6d9a4295463bb3ffcfb3344a6865f8a95e
SURFACEPRO4\bmabi <bmabir17@gmail.com>
2017-03-14 20:45:38 +0600

    report card update

---
+++ day3/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="utf-8">
+<head>
+	
+	  <meta charset="utf-8">
+</head>>
+<body style ="background:rgba(150,0,0,10)">
+   
+
+
+
+   <a href="http://www.google.com" target="_blank" title="Dina">google loves dina</a> <!--use only _blank and _parent : _blank opens in new tab , _parent opens in parent frame
+   tooltip use korte hobe -->
+   <h1>Loru's Bitlami</h1>
+   <b>muhahaha</b>
+   <br>
+   <strong>khaisi toreee</strong>
+   <br>
+   <i> DINAA</i>
+   <br>
+   <em>emphasized txt</em>
+   <br>
+   <mark> marked txt </mark>
+   <br>
+   <small> small text </small>
+   <br>
+   <del> deleted txt</del>
+   <br>
+   <ins>instant txt </ins>
+   <br>
+   <sub>subscript txt</sub>
+   <br>
+   <sup>superscript</sup>
+   </br>
+
+    <p title="this is a tooltip ">
+    	some txt 
+    </p>
+
+<img src="loru.PNG" width="350" height="400">
+
+  
+
+
+
+  </body>
+  </html>
