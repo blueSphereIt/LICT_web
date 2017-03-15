9d1379620362a6e270274d1f611d533998c939f1
SURFACEPRO4\bmabi <bmabir17@gmail.com>
2017-03-15 21:49:46 +0600

    report Card v1

---
+++ abir/report_card/index.php
@@ -0,0 +1,364 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Report Card</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div>
+	<table border="1" style="width=100%; ">
+		
+		
+		<col width="160">
+		<!-- 1st row -->
+		<tr>
+			<td colspan="3" rowspan="5" style="text-align:left "><img src="pic.jpg" width="100%" ></td>
+			<th colspan="2">Name</th>
+			<td colspan="3">Nafisa Akter jarin</td>
+			<th colspan="2">ID No</th>
+			<td colspan="3">16-Eight-provaty-1</td>
+
+			<th colspan="3" >Behavior</th> 
+			<th colspan="4" >Co-Curricular</th> 
+			<th colspan="3" >Performace in the class</th> 
+		</tr>
+		<!-- 2nd row -->
+		<tr>
+			
+			<th colspan="2">Father's Name</th>
+			<td colspan="3">Badol Mia</td>
+			<th colspan="2">Code No</th>
+			<td colspan="3">201612112331</td>
+
+			<th colspan="2">Excellent</th> 
+			<td> &emsp;</td>
+			<th colspan="3">Sports</th>
+			<td> &emsp;</td> 
+			<th colspan="2">Excellent</th> 
+			<td>&emsp;</td>
+		</tr>
+		<!-- 3rd row -->
+		<tr>
+			
+			<th colspan="2">Mother's Name</th>
+			<td colspan="3">Parvin Begum</td>
+			<th colspan="2">Class</th>
+			<td colspan="3">Eight</td>
+
+			<th colspan="2">Very Good</th> 
+			<td> &emsp;</td>
+			<th colspan="3">Cultaral Function</th>
+			<td> &emsp;</td> 
+			<th colspan="2">Satisfactory</th> 
+			<td> &emsp;</td>
+		</tr>
+		<!-- 4th row -->
+		<tr>
+			
+			<th colspan="2">Shift</th>
+			<td colspan="3">Morning</td>
+			<th colspan="2">Section</th>
+			<td colspan="3">Morning</td>
+
+			<th colspan="2">Good</th> 
+			<td>&emsp; </td>
+			<th colspan="3">Girls Guide/Yellow Bird</th>
+			<td>&emsp; </td> 
+			<th colspan="2">Average</th> 
+			<td> &emsp;</td>
+		</tr>
+		<!-- 5th row -->
+		<tr>
+			
+			<th colspan="2">Version</th>
+			<td colspan="3">Bangla Medium</td>
+			<th colspan="2">Roll</th>
+			<td colspan="3">1</td>
+
+			<th colspan="2">Need Improvement</th> 
+			<td> &emsp;</td>
+			<th colspan="3">Demate/Math/Scienc<br> Olympiad</th>
+			<td> &emsp;</td> 
+			<th colspan="2">Unsatisfactory</th> 
+			<td>&emsp; </td>
+		</tr>
+
+	</table>
+
+	<table border="1" style="width=100%">
+		<!-- First row -->
+		<tr>
+			<th colspan="6" rowspan="3">Subject Name</th>
+			<th colspan="2" rowspan="2">CT/CA</th>
+			<th colspan="6">Term Exam</th>
+			<th colspan="2" rowspan="3">Obtained <br> (Sub+Obj<br>+Prac)<br> Total Marks</th>
+			<th rowspan="3">Convt 80% Marks</th>
+			<th rowspan="3">Total Marks</th>
+			<th rowspan="3">Highest Marks</th>
+			<th rowspan="3">Letter Grade</th>
+			<th rowspan="3">Grade Point</th>
+			<th rowspan="3" colspan="2">GPA &emsp;</th>
+		</tr>
+		<!-- 2nd row -->
+		<tr>
+			<th colspan="2">Subjective</th>
+			<th colspan="2">Objective</th>
+			<th colspan="2">Practical</th>
+		</tr>
+		<!-- 3rd row -->
+		<tr>
+			<th>Total</th>
+			<th>Obt.</th>
+			<th>Total</th>
+			<th>Obt.</th>
+			<th>Total</th>
+			<th>Obt.</th>
+			<th>Total</th>
+			<th>Obt.</th>
+		</tr>
+		<!-- 4th row -->
+		<tr>
+			<th colspan="6"> Bangla 1st paper</th>
+			<td></td>
+			<td></td>
+			<td>60</td>
+			<td>46</td>
+			<td>40</td>
+			<td>25</td>
+			<td></td>
+			<td></td>
+			<td colspan="2">71</td>
+			<td>71</td>
+			<td>71</td>
+			<td>78</td>
+			<td>A</td>
+			<td>4.00</td>
+			<td rowspan="14">4.70</td>
+		</tr>
+		<!-- 5th row -->
+		<tr>
+			<th colspan="6"> Bangla 2nd paper</th>
+			<td></td>
+			<td></td>
+			<td>50</td>
+			<td>37</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td colspan="2">37</td>
+			<td>37</td>
+			<td>37</td>
+			<td>40</td>
+			<td></td>
+			<td></td>
+		</tr>
+		<!-- 6th row -->
+		<tr>
+			<th colspan="6"> English 1st paper</th>
+			<td></td>
+			<td></td>
+			<td>50</td>
+			<td>37</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td colspan="2">37</td>
+			<td>37</td>
+			<td>37</td>
+			<td>40</td>
+			<td>A-</td>
+			<td>3.5</td>
+		</tr>
+		<!-- 7th row -->
+		<tr>
+			<th colspan="6"> English 2nd paper</th>
+			<td></td>
+			<td></td>
+			<td>50</td>
+			<td>37</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td colspan="2">37</td>
+			<td>37</td>
+			<td>37</td>
+			<td>40</td>
+			<td></td>
+			<td></td>
+		</tr>
+		<!-- 8th row -->
+		<tr>
+			<th colspan="6"> General Maths</th>
+			<td></td>
+			<td></td>
+			<td>50</td>
+			<td>37</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td colspan="2">37</td>
+			<td>37</td>
+			<td>37</td>
+			<td>40</td>
+			<td>A-</td>
+			<td>3.5</td>
+		</tr>
+		<!-- 9th row -->
+		<tr>
+			<th colspan="6"> Religion & Moral Education</th>
+			<td></td>
+			<td></td>
+			<td>50</td>
+			<td>37</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td colspan="2">37</td>
+			<td>37</td>
+			<td>37</td>
+			<td>40</td>
+			<td>A-</td>
+			<td>3.5</td>
+		</tr>
+		<!-- 10th row -->
+		<tr>
+			<th colspan="6"> General Science</th>
+			<td></td>
+			<td></td>
+			<td>50</td>
+			<td>37</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td colspan="2">37</td>
+			<td>37</td>
+			<td>37</td>
+			<td>40</td>
+			<td>A-</td>
+			<td>3.5</td>
+		</tr>
+		<!-- 11th row -->
+		<tr>
+			<th colspan="6"> Bangladesh and Global Studies</th>
+			<td></td>
+			<td></td>
+			<td>50</td>
+			<td>37</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td colspan="2">37</td>
+			<td>37</td>
+			<td>37</td>
+			<td>40</td>
+			<td>A-</td>
+			<td>3.5</td>
+		</tr>
+		<!-- 12th row -->
+		<tr>
+			<th colspan="6">Physical Education and Health</th>
+			<td></td>
+			<td></td>
+			<td>50</td>
+			<td>37</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td colspan="2">37</td>
+			<td>37</td>
+			<td>37</td>
+			<td>40</td>
+			<td>A-</td>
+			<td>3.5</td>
+		</tr>
+		<!-- 13th row -->
+		<tr>
+			<th colspan="6"> Information & Communication <br> Studies</th>
+			<td></td>
+			<td></td>
+			<td>50</td>
+			<td>37</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td colspan="2">37</td>
+			<td>37</td>
+			<td>37</td>
+			<td>40</td>
+			<td>A-</td>
+			<td>3.5</td>
+		</tr>
+		<!-- 14th row -->
+		<tr>
+			<th colspan="6"> Arts & Crafts</th>
+			<td></td>
+			<td></td>
+			<td>50</td>
+			<td>37</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td colspan="2">37</td>
+			<td>37</td>
+			<td>37</td>
+			<td>40</td>
+			<td>A-</td>
+			<td>3.5</td>
+		</tr>
+		<!-- 15th row -->
+		<tr>
+			<th colspan="6">work & Life oriented Education </th>
+			<td></td>
+			<td></td>
+			<td>50</td>
+			<td>37</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td colspan="2">37</td>
+			<td>37</td>
+			<td>37</td>
+			<td>40</td>
+			<td>A-</td>
+			<td>3.5</td>
+		</tr>
+		<!-- 16th row -->
+		<tr>
+			<th colspan="6"> Home Science</th>
+			<td></td>
+			<td></td>
+			<td>50</td>
+			<td>37</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td colspan="2">37</td>
+			<td>37</td>
+			<td>37</td>
+			<td>40</td>
+			<td>A-</td>
+			<td>3.5</td>
+		</tr>
+		<!-- 17th row -->
+		<tr>
+			<th colspan="6"> Grand Total</th>
+			<td colspan="10" style="text-align: right; padding-right: 30px ">784</td>
+				<td colspan="5"></td> 		</tr>
+
+
+	</table>
+</div>
+</body>
+</html>
